034bc25724d7e8d9902be7f075b0663e740ef230
Theo9696 <theo.dolle@student.ecp.fr>
2019-11-13 18:37:20 +0100

    Updates

---
+++ precache-manifest.99a3fa6154c5e822d21f06e94c17f18e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "939e6755d616504b83dbcbd272a908d6",
+    "url": "/blindtest-spotify/index.html"
+  },
+  {
+    "revision": "4497bf1a01c1775afbaf",
+    "url": "/blindtest-spotify/static/css/main.9651b04e.chunk.css"
+  },
+  {
+    "revision": "cdbe789fea79615bfa14",
+    "url": "/blindtest-spotify/static/js/2.3df8ad22.chunk.js"
+  },
+  {
+    "revision": "4497bf1a01c1775afbaf",
+    "url": "/blindtest-spotify/static/js/main.d221b54c.chunk.js"
+  },
+  {
+    "revision": "d74675ac0a498334cc92",
+    "url": "/blindtest-spotify/static/js/runtime-main.07f395c8.js"
+  },
+  {
+    "revision": "ed34bc26e2e33c49af23c6c44c6bdee9",
+    "url": "/blindtest-spotify/static/media/loading.ed34bc26.svg"
+  },
+  {
+    "revision": "6a5812940578a284a5256eab66eddac5",
+    "url": "/blindtest-spotify/static/media/logo.6a581294.svg"
+  }
+]);
