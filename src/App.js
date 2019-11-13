99cd7df5f9cfb733b5d6dda03f00737f45590da7
Theo9696 <theo.dolle@student.ecp.fr>
2019-11-13 19:19:14 +0100

    [Step2] finish

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 import Sound from 'react-sound';
 import Button from './Button';
 
-const apiToken = '<<Copiez le token de Spotify ici>>';
+const apiToken = 'BQBj8nzALqnYA3TJUIyKEln6O4O6xO3-GSoOPbjC8__UcNyece6mTjrKbEtVwrI7vP5dQelfcWJK6MjFi1OqfTpYjnLpTfsM_9NGYJHp0VIV8E3OkfD-PTNsQuWFcGYk2eLcZ4B1KsOhVrS2UmJdFI_cdhfrVoRHaH1TW6pfkw';
 
 function shuffleArray(array) {
   let counter = array.length;
@@ -32,6 +32,27 @@ class App extends Component {
 
   constructor() {
     super();
+    this.state = {
+      text: "",
+      data: {},
+      songLoaded: false
+    };
+  }
+
+  async componentDidMount() {
+    this.setState({ text: "Bonjour" });
+
+    const res = await fetch('https://api.spotify.com/v1/me/tracks', {
+      method: 'GET',
+      headers: {
+      Authorization: 'Bearer ' + apiToken,
+      },
+    })
+    .then(response => response.json());
+
+    console.log(res);
+    this.setState({ data: res, songLoaded: true });
+
   }
 
   render() {
@@ -42,7 +63,14 @@ class App extends Component {
           <h1 className="App-title">Bienvenue sur le Blindtest</h1>
         </header>
         <div className="App-images">
-          <p>Faisons un vrai Blindtest !</p>
+          {this.state.songLoaded ?
+          <div>
+            <p>{this.state.text}</p>
+            <p>Nombre de musiques + {this.state.data.items.length}</p>
+            {this.state.data.items ? <p>Titre première chanson + {this.state.data.items[0].track.name}</p> : null}
+          </div>
+          : 
+          <img src={loading} className="Logo-loarding" alt="loading"/>}
         </div>
         <div className="App-buttons">
         </div>
